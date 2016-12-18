5438fc9714c9fc1c3a9e6f98f58fd6a7b5285174
Boey <khcy5bjw@nottingham.edu.my>
2016-12-18 21:50:13 +0800

    Added overlay on top of the map. Currently pinpoint accurate.

--- src/com/neet/DiamondHunter/MapViewer/MapViewController.java
+++ src/com/neet/DiamondHunter/MapViewer/MapViewController.java
@@ -6,14 +6,14 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.TilePane;
 
 public class MapViewController implements Initializable {
 	
 	@FXML
 	private Canvas mvCanvas;
 	@FXML
-	private GridPane mapCoordinates;
+	private TilePane tileMapping;
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
@@ -22,5 +22,12 @@ public class MapViewController implements Initializable {
 		mvCanvas.setWidth((double) (MapPane.WIDTH));
 		mvCanvas.setHeight((double) (MapPane.HEIGHT));
 		mvCanvas.getGraphicsContext2D().drawImage(mp.getCanvas(), 0, 0);
+		
+		tileMapping.relocate(mvCanvas.getLayoutX() + 1, mvCanvas.getLayoutY() - 2);
+		tileMapping.setPrefSize((double) (mp.getNumCols() * mp.getTileSize()), (double) (mp.getNumCols() * mp.getTileSize()));
+		tileMapping.setPrefTileWidth((double) (mp.getTileSize()));
+		tileMapping.setPrefTileHeight((double) (mp.getTileSize()));
+		tileMapping.setPrefRows(mp.getNumRows());
+		tileMapping.setPrefColumns(mp.getNumCols());
 	}
 }
